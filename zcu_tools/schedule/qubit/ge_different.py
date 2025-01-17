74b84a326071d28878a0d8ce0d4a6beb8ecba667
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2025-01-18 01:13:12 +0800

    Revert "add second cutoff to ge diff"
    This reverts commit 03793cb5c5a7728a6980e505c3a46212860e55ba.

--- zcu_tools/schedule/qubit/ge_different.py
+++ zcu_tools/schedule/qubit/ge_different.py
@@ -80,7 +80,7 @@ def measure_ge_pdr_dep(
     return fpts, pdrs, snr2D  # (pdrs, freqs)
 
 
-def measure_ge_ro_dep(soc, soccfg, cfg, instant_show=False, cutoff=(0.0, np.inf)):
+def measure_ge_ro_dep(soc, soccfg, cfg, instant_show=False, cutoff=np.inf):
     cfg = deepcopy(cfg)  # prevent in-place modification
 
     set_flux(cfg["flux_dev"], cfg["flux"])
@@ -108,7 +108,7 @@ def measure_ge_ro_dep(soc, soccfg, cfg, instant_show=False, cutoff=(0.0, np.inf)
             for j, offset in enumerate(offsets):
                 cfg["adc"]["trig_offset"] = offset
 
-                if cutoff[0] <= offset + ro_len <= cutoff[1]:
+                if offset + ro_len <= cutoff:
                     snr2D[i, j] = measure_one(soc, soccfg, cfg)
                 trig_tqdm.update()
 
