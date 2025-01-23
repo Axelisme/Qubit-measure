3db16229db01fa794148a02cc778c0759d9645ca
Axel-Chiu <jary33333@gapp.nthu.edu.tw>
2025-01-23 16:46:35 +0800

    remove unworkable RFreqOnetone

--- zcu_tools/schedule/resonator/onetone.py
+++ zcu_tools/schedule/resonator/onetone.py
@@ -4,13 +4,13 @@ import numpy as np
 from tqdm.auto import tqdm
 
 from zcu_tools import make_cfg
-from zcu_tools.program2 import OneToneProgram, RFreqOnetoneProgram
+from zcu_tools.program2 import OneToneProgram
 
 from ..flux import set_flux
 from ..instant_show import clear_show, init_show, update_show
 
 
-def measure_res_freq(soc, soccfg, cfg, instant_show=False, soft_loop=False):
+def measure_res_freq(soc, soccfg, cfg, instant_show=False):
     cfg = deepcopy(cfg)  # prevent in-place modification
 
     set_flux(cfg["flux_dev"], cfg["flux"])
@@ -19,7 +19,6 @@ def measure_res_freq(soc, soccfg, cfg, instant_show=False, soft_loop=False):
     if isinstance(sweep_cfg, dict):
         fpts = np.linspace(sweep_cfg["start"], sweep_cfg["stop"], sweep_cfg["expts"])
     else:
-        assert soft_loop, "Hard loop only supports linear sweep"
         fpts = np.array(sweep_cfg)
 
     res_pulse = cfg["dac"]["res_pulse"]
@@ -28,25 +27,17 @@ def measure_res_freq(soc, soccfg, cfg, instant_show=False, soft_loop=False):
     if instant_show:
         fig, ax, dh, curve = init_show(fpts, "Frequency (MHz)", "Amplitude")
 
+    print("Use OneToneProgram for soft loop")
     signals = np.full(len(fpts), np.nan, dtype=np.complex128)
-    if soft_loop:
-        print("Use OneToneProgram for soft loop")
-        for i, fpt in enumerate(tqdm(fpts, desc="Frequency", smoothing=0)):
-            res_pulse["freq"] = fpt
-            prog = OneToneProgram(soccfg, make_cfg(cfg))
-            avgi, avgq = prog.acquire(soc, progress=False)
-            signals[i] = avgi[0][0] + 1j * avgq[0][0]
+    for i, fpt in enumerate(tqdm(fpts, desc="Frequency", smoothing=0)):
+        res_pulse["freq"] = fpt
+        prog = OneToneProgram(soccfg, make_cfg(cfg))
+        avgi, avgq = prog.acquire(soc, progress=False)
+        signals[i] = avgi[0][0] + 1j * avgq[0][0]
 
-            if instant_show and i % show_period == 0:
-                update_show(fig, ax, dh, curve, np.abs(signals))
-        else:
-            if instant_show:
-                update_show(fig, ax, dh, curve, np.abs(signals))
+        if instant_show and i % show_period == 0:
+            update_show(fig, ax, dh, curve, np.abs(signals))
     else:
-        print("Use RFreqOnetoneProgram for hard loop")
-        prog = RFreqOnetoneProgram(soccfg, make_cfg(cfg))
-        fpts, avgi, avgq = prog.acquire(soc, progress=True)
-        signals = avgi[0][0] + 1j * avgq[0][0]
         if instant_show:
             update_show(fig, ax, dh, curve, np.abs(signals))
 
