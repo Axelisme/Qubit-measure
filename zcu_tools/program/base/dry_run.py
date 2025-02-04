2e33ed62bfcde14df33874554580e6e8e20ea755
Axelisme <jary33333@gapp.nthu.edu.tw>
2025-02-04 17:02:54 +0800

    fix dry run

--- zcu_tools/program/base/dry_run.py
+++ zcu_tools/program/base/dry_run.py
@@ -1,7 +1,7 @@
-import time
-
 import numpy as np
 from tqdm.auto import trange
+import Pyro4
+import time
 
 
 class DryRunProgram:
@@ -93,49 +93,80 @@ class DryRunProgram:
     def measure(self, *args, **kwargs):
         pass
 
-    def acquire_avg(self, soc, progress, round_callback, callback_period):
+    def _run_callback(self, callback, *args, **kwargs):
+        if callable(callback):
+            # local callback
+            callback(*args, **kwargs)
+        else:
+            # remote callback
+            callback._pyroTimeout = 1.0  # s
+            try:
+                callback.oneway_callback(*args, **kwargs)
+            except Pyro4.errors.CommunicationError as e:
+                print("Callback failed:", e)
+
+    def acquire_avg(
+        self, soc, progress, round_callback, callback_period, ret_std=False
+    ):
         # AveragerProgram
         avg_d = [np.zeros((1, 2))]
+        std2_d = [np.ones((1, 2))]
         for ir in trange(self.soft_avgs, leave=False, disable=not progress):
             avg_d[0][0] += np.mean(np.random.rand(self.cfg["reps"], 2), axis=0)
-            if round_callback is not None and ir % callback_period == 0:
-                cur_avg = [d / (ir + 1) for d in avg_d]
-                if callable(round_callback):
-                    round_callback(ir, cur_avg)
-                else:
-                    round_callback.oneway_callback(ir, cur_avg)
-            time.sleep(0.01)
+            if round_callback is not None and (
+                (ir + 1) % callback_period == 0 or ir == self.soft_avgs - 1
+            ):
+                cur_avg = [d / max(1, ir) for d in avg_d]
+                self._run_callback(round_callback, ir, cur_avg)
+                time.sleep(0.1)
         for d in avg_d:
             d /= self.soft_avgs
+
         avg_i = [d[..., 0] for d in avg_d]
         avg_q = [d[..., 1] for d in avg_d]
-        return avg_i, avg_q
+        if ret_std:
+            std2_i = [d[..., 0] for d in std2_d]
+            std2_q = [d[..., 1] for d in std2_d]
+            return avg_i, avg_q, std2_i, std2_q
+        else:
+            return avg_i, avg_q
 
-    def acquire_ravg(self, soc, progress, round_callback, callback_period):
+    def acquire_ravg(
+        self, soc, progress, round_callback, callback_period, ret_std=False
+    ):
         # RAveragerProgram
         xs = self.cfg["start"] + np.arange(self.cfg["expts"]) * self.cfg["step"]
         ys = np.sin(xs / (xs.max() - xs.min()) * 2 * np.pi) * 0.1
         ys = np.stack((ys, ys), axis=-1)
         avg_d = [np.zeros((1, len(xs), 2))]
+        std2_d = [np.ones((1, len(xs), 2))]
         for ir in trange(self.soft_avgs, leave=False, disable=not progress):
             y = ys + np.mean(np.random.rand(self.cfg["reps"], len(xs), 2), axis=0)
             avg_d[0][0] += y
-            if round_callback is not None and ir % callback_period == 0:
-                cur_avg = [d / (ir + 1) for d in avg_d]
-                if callable(round_callback):
-                    round_callback(ir, cur_avg)
-                else:
-                    round_callback.oneway_callback(ir, cur_avg)
-            time.sleep(0.1)
+            if round_callback is not None and (
+                (ir + 1) % callback_period == 0 or ir == self.soft_avgs - 1
+            ):
+                cur_avg = [d / max(1, ir) for d in avg_d]
+                self._run_callback(round_callback, ir, cur_avg)
+                time.sleep(0.1)
+
         for d in avg_d:
             d /= self.soft_avgs
+
         avg_i = [d[..., 0] for d in avg_d]
         avg_q = [d[..., 1] for d in avg_d]
-        return xs, avg_i, avg_q
+        if ret_std:
+            std2_i = [d[..., 0] for d in std2_d]
+            std2_q = [d[..., 1] for d in std2_d]
+            return xs, avg_i, avg_q, std2_i, std2_q
+        else:
+            return xs, avg_i, avg_q
 
     def acquire(
         self, soc, progress=False, round_callback=None, callback_period=100, **kwargs
     ):
+        self.di_buf = [np.random.randn(self.cfg["expts"] * self.cfg["reps"])]
+        self.dq_buf = [np.random.randn(self.cfg["expts"] * self.cfg["reps"])]
         if hasattr(self, "update"):  # RAveragerProgram
             return self.acquire_ravg(
                 soc,
@@ -154,7 +185,7 @@ class DryRunProgram:
             )
 
     def acquire_decimated(
-        self, soc, progress=False, round_callback=None, callback_period=100
+        self, soc, progress=False, round_callback=None, callback_period=100, **kwargs
     ):
         # compare to acquire_avg, it have one more dimension at the end
         num = soc.us2cycles(3.32)
@@ -164,13 +195,11 @@ class DryRunProgram:
         for ir in trange(self.soft_avgs, leave=False, disable=not progress):
             y = ys + np.mean(np.random.rand(self.cfg["reps"], num, 2), axis=0)
             avg_d += y
-            time.sleep(0.01)
-            if round_callback is not None and ir % callback_period == 0:
-                cur_avg = [d / (ir + 1) for d in avg_d]
-                if callable(round_callback):
-                    round_callback(ir, cur_avg)
-                else:
-                    round_callback.oneway_callback(ir, cur_avg)
+            if round_callback is not None and (
+                (ir + 1) % callback_period == 0 or ir == self.soft_avgs - 1
+            ):
+                self._run_callback(round_callback, ir)
+                time.sleep(0.1)
         avg_d /= self.soft_avgs
 
         avg_i = [d[..., 0] for d in avg_d]
