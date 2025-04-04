04968503f105eaa08f6862031c0f06db4b96d12a
Axelisme <jary33333@gmail.com>
2025-04-04 23:19:32 +0800

    add earlystop snr to v1 rabi

--- lib/zcu_tools/schedule/v1/qubit/rabi.py
+++ lib/zcu_tools/schedule/v1/qubit/rabi.py
@@ -2,7 +2,12 @@ from copy import deepcopy
 
 import numpy as np
 
-from zcu_tools.analysis import minus_background
+from zcu_tools.analysis import (
+    minus_background,
+    calculate_noise,
+    rotate2real,
+    peak_n_avg,
+)
 from zcu_tools.program.v1 import RGainTwoToneProgram, TwoToneProgram
 from zcu_tools.schedule.tools import sweep2array
 from zcu_tools.schedule.v1.template import sweep1D_hard_template, sweep1D_soft_template
@@ -12,8 +17,22 @@ def signal2real(signals: np.ndarray) -> np.ndarray:
     return np.abs(minus_background(signals))
 
 
-def measure_lenrabi(soc, soccfg, cfg):
+def qub_signal2snr(signals):
+    noise, m_signals = calculate_noise(signals)
+
+    m_real = rotate2real(m_signals).real
+    contrast = peak_n_avg(m_real, n=3, mode="max") - peak_n_avg(m_real, n=3, mode="min")
+
+    return contrast / noise
+
+
+def measure_lenrabi(soc, soccfg, cfg, *, force_align=False, earlystop_snr=None):
     cfg = deepcopy(cfg)
+
+    if force_align:
+        raise NotImplementedError("force_align is not implemented for v1 yet")
+    if earlystop_snr is not None:
+        raise NotImplementedError("earlystop_snr is not implemented for v1 yet")
 
     lens = sweep2array(cfg["sweep"], allow_array=True)
 
@@ -37,10 +56,19 @@ def measure_lenrabi(soc, soccfg, cfg):
     return lens, signals
 
 
-def measure_amprabi(soc, soccfg, cfg):
+def measure_amprabi(soc, soccfg, cfg, *, earlystop_snr=None):
     cfg = deepcopy(cfg)
 
     pdrs = sweep2array(cfg["sweep"])
+
+    if earlystop_snr is not None:
+
+        def checker(signals):
+            snr = qub_signal2snr(signals)
+            return snr >= earlystop_snr, f"SNR: {snr:.2f}"
+
+    else:
+        checker = None
 
     pdrs, signals = sweep1D_hard_template(
         soc,
@@ -51,6 +79,7 @@ def measure_amprabi(soc, soccfg, cfg):
         xlabel="Pulse Power (a.u.)",
         ylabel="Amplitude",
         signal2real=signal2real,
+        early_stop_checker=checker,
     )
 
     return pdrs, signals
