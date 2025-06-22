a8315d0c3b5fc8845a566f1b6f546a7d328a6298
Axelisme <jary33333@gmail.com>
2025-06-22 23:48:29 +0800

    add new yoko

--- lib/zcu_tools/device/__init__.py
+++ lib/zcu_tools/device/__init__.py
@@ -1,3 +1,16 @@
-from .labber import YokoDevControl
+from typing import Any
 
-__all__ = ["YokoDevControl"]
+
+class GlobalDeviceManager:
+    devices = {}
+
+    def register_device(self, name: str, device: Any) -> None:
+        self.devices[name] = device
+
+    def get_device(self, name: str) -> Any:
+        if name not in self.devices:
+            raise ValueError(f"Device {name} not found")
+        return self.devices[name]
+
+    def get_all_devices(self) -> dict[str, Any]:
+        return self.devices
