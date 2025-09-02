b994cf77b4eec5041318d31ac999bf0f90eedfd6
Axelisme <jary33333@gmail.com>
2025-09-02 16:40:17 +0800

    fix import error

--- lib/zcu_tools/notebook/analysis/design/__init__.py
+++ lib/zcu_tools/notebook/analysis/design/__init__.py
@@ -6,7 +6,6 @@ from .search import (
     avoid_low_f01,
     avoid_low_m01,
     calculate_chi_sweep,
-    calculate_dipersive_shift,
     calculate_dispersive,
     calculate_esys,
     calculate_f01,
