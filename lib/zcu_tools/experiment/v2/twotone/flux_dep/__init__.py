f12749c69010cca5f885ba262ac4d51f6eee2a3a
Axelisme <jary33333@gmail.com>
2025-09-09 20:55:50 +0800

    let wrap_pulse_with_flux_pulse replace derive_flux_pulse

--- lib/zcu_tools/experiment/v2/twotone/flux_dep/__init__.py
+++ lib/zcu_tools/experiment/v2/twotone/flux_dep/__init__.py
@@ -1,11 +1,11 @@
 from .freq import FreqExperiment
 from .t1 import T1Experiment
 from .t2ramsey import T2RamseyExperiment
-from .util import derive_flux_pulse_from_pulse
+from .util import wrap_with_flux_pulse
 
 __all__ = [
     "T1Experiment",
     "T2RamseyExperiment",
     "FreqExperiment",
-    "derive_flux_pulse_from_pulse",
+    "wrap_with_flux_pulse",
 ]
