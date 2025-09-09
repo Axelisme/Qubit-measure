e5066d792c91497df230931a125bc08741fd3ce1
Axelisme <jary33333@gmail.com>
2025-09-09 20:55:02 +0800

    add t and post_delay to auto_derive

--- lib/zcu_tools/library.py
+++ lib/zcu_tools/library.py
@@ -32,9 +32,11 @@ def auto_derive_module(
         module_cfg = deepcopy(ml.get_module(name))
     module_cfg["name"] = name
 
-    # if it also a pulse cfg
-    if "style" in module_cfg:
+    # if it also a pulse cfg, exclude raise_pulse in flat_top
+    if "style" in module_cfg and name != "raise_pulse":
         module_cfg.setdefault("phase", 0.0)
+        module_cfg.setdefault("t", "auto")
+        module_cfg.setdefault("post_delay", 0.0)
 
     # derive pulse in module
     for key, value in module_cfg.items():
