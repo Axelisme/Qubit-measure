4ba9dd97fefad30d0a098beb0ce863a00cf00979
Axelisme <jary33333@gmail.com>
2025-09-11 15:22:43 +0800

    add flx_lookback exp

--- lib/zcu_tools/experiment/v2/twotone/flux_dep/__init__.py
+++ lib/zcu_tools/experiment/v2/twotone/flux_dep/__init__.py
@@ -1,11 +1,11 @@
 from .freq import FreqExperiment
 from .t1 import T1Experiment
 from .t2ramsey import T2RamseyExperiment
-from .util import wrap_with_flux_pulse
+from .flx_lookback import FluxLookbackExperiment
 
 __all__ = [
     "T1Experiment",
     "T2RamseyExperiment",
     "FreqExperiment",
-    "wrap_with_flux_pulse",
+    "FluxLookbackExperiment",
 ]
---
+++ lib/zcu_tools/experiment/v2/twotone/flux_dep/flx_lookback.py
@@ -0,0 +1,130 @@
+from __future__ import annotations
+
+from copy import deepcopy
+from typing import Any, Callable, Dict, Optional, Tuple
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from zcu_tools.experiment import AbsExperiment
+from zcu_tools.experiment.utils import sweep2array
+from zcu_tools.liveplot import LivePlotter2D
+from zcu_tools.program.v2 import (
+    ModularProgramV2,
+    Pulse,
+    make_readout,
+    make_reset,
+    sweep2param,
+)
+from zcu_tools.utils.datasaver import save_data
+from zcu_tools.utils.process import rotate2real
+from zcu_tools.utils.fitting import fit_decay
+
+from ...template import sweep_hard_template
+from .util import check_flux_pulse, check_no_post_delay
+
+FluxLookbackResultType = Tuple[np.ndarray, np.ndarray, np.ndarray]
+
+
+def fluxlookback_signal2real(signals: np.ndarray) -> np.ndarray:
+    return rotate2real(signals).real
+
+
+class FluxLookbackExperiment(AbsExperiment[FluxLookbackResultType]):
+    def run(
+        self, soc, soccfg, cfg: Dict[str, Any], *, progress: bool = True
+    ) -> FluxLookbackResultType:
+        cfg = deepcopy(cfg)  # prevent in-place modification
+
+        flx_pulse = cfg["flx_pulse"]
+        qub_pulse = cfg["qub_pulse"]
+        check_flux_pulse(flx_pulse)
+        check_no_post_delay(qub_pulse, "qub_pulse")
+
+        # let length be outer loop
+        cfg["sweep"] = {
+            "length": cfg["sweep"]["length"],
+            "freq": cfg["sweep"]["freq"],
+        }
+
+        # predict point
+        lens = sweep2array(cfg["sweep"]["length"])
+        fpts = sweep2array(cfg["sweep"]["freq"])
+
+        # swept by FPGA (hard sweep)
+        qub_pulse["t"] = sweep2param("length", cfg["sweep"]["length"])
+        qub_pulse["freq"] = sweep2param("freq", cfg["sweep"]["freq"])
+
+        prog = ModularProgramV2(
+            soccfg,
+            cfg,
+            modules=[
+                make_reset("reset", reset_cfg=cfg.get("reset")),
+                Pulse(name="pi_pulse", cfg=cfg["pi_pulse"]),
+                Pulse(name="flux_pulse", cfg=cfg["flx_pulse"]),
+                make_readout("readout", readout_cfg=cfg["readout"]),
+            ],
+        )
+
+        def measure_fn(
+            _: Dict[str, Any], cb: Optional[Callable[..., None]]
+        ) -> np.ndarray:
+            return prog.acquire(soc, progress=True, callback=cb)[0][0].dot([1, 1j])
+
+        # Run 2D soft-hard sweep (flux soft, length hard)
+        signals2D = sweep_hard_template(
+            cfg,
+            measure_fn,
+            LivePlotter2D(
+                "Time (us)",
+                "Frequency (MHz)",
+                disable=not progress,
+            ),
+            ticks=(lens, fpts),
+            signal2real=fluxlookback_signal2real,
+            progress=progress,
+        )
+
+        # Get the actual frequency points used by FPGA
+        real_lens = prog.get_time_param("qub_pulse", as_array=True)
+        assert isinstance(real_lens, np.ndarray), "fpts should be an array"
+        real_lens += lens[0] - real_lens[0]  # correct absolute offset
+
+        # Cache results
+        self.last_cfg = cfg
+        self.last_result = (real_lens, fpts, signals2D)
+
+        return real_lens, fpts, signals2D
+
+    def analyze(self, result: Optional[FluxLookbackResultType] = None) -> None:
+        if result is None:
+            result = self.last_result
+        assert result is not None, "no result found"
+
+        # lens, fpts, signals2D = result
+
+        raise NotImplementedError("analysis not implemented yet")
+
+    def save(
+        self,
+        filepath: str,
+        result: Optional[FluxLookbackResultType] = None,
+        comment: Optional[str] = None,
+        tag: str = "twotone/flux_dep/flx_lookback",
+        **kwargs,
+    ) -> None:
+        if result is None:
+            result = self.last_result
+        assert result is not None, "no result found"
+
+        lens, fpts, signals2D = result
+
+        save_data(
+            filepath=filepath,
+            x_info={"name": "Time", "unit": "s", "values": lens * 1e-6},
+            y_info={"name": "Frequency", "unit": "Hz", "values": fpts * 1e6},
+            z_info={"name": "Signal", "unit": "a.u.", "values": signals2D.T},
+            comment=comment,
+            tag=tag,
+            **kwargs,
+        )
