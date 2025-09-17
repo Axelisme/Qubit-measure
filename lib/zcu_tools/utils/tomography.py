3a92e62b99d5f3d811c5933e95db26e89afffa1c
JayChien04717 <zx123c123@gmail.com>
2025-09-17 13:46:32 +0800

    add normaliz_signal to util tomography

--- lib/zcu_tools/utils/tomography.py
+++ lib/zcu_tools/utils/tomography.py
@@ -7,7 +7,14 @@ def xyz2sphere(x, y, z):
     z = np.asarray(z)
 
     r = np.sqrt(x**2 + y**2 + z**2)
-    theta = np.arccos(z / r)  # polar angle
-    phi = np.arctan2(y, x)  # azimuthal angle
+    theta = np.arccos(z / r) * 180 / np.pi - 90  # polar angle in degrees
+    phi = np.arctan2(y, x) * 180 / np.pi  # azimuthal angle in degrees
 
     return r, theta, phi
+
+
+def normalize_signal(x):
+    x = np.asarray(x)
+    x_min, x_max = np.min(x), np.max(x)
+    x_center = (x_max + x_min) / 2
+    return 2 * (x - x_center) / (x_max - x_min)
